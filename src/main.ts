426e9b1c18f3f10c90d418755c8606a0f5bd3682
JustaFramework <wachirawamunyi@gmail.com>
2021-02-22 23:43:39 +0200

    nodemon and etc

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,10 @@ async function bootstrap() {
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
 
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  }
+
   await app.listen(port);
 
   logger.log(`Application listening on port ${port}`);
